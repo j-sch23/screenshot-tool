646d1e089011e8b8ad736c8879307230707bc6f9
skrt-dev <jacobschwantes@outlook.com>
2021-09-11 13:35:12 -0500

    more options

--- screenshot-tool/pages/index.js
+++ screenshot-tool/pages/index.js
@@ -1,131 +1,248 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dropdown from "../components/Dropdown";
-import InputURL from "../components/InputURL";
+import InputURL from "../components/InputWithURL";
 import Toggle from "../components/Toggle";
 import BasicInput from "../components/BasicInput";
-import { PlusIcon as PlusIconSolid } from "@heroicons/react/solid";
+import Radio from "../components/Radio";
 export default function Home() {
-  const [img, setImg] = useState("default_img.png");
   const [url, setUrl] = useState("www.example.com");
-  const [loading, setLoad] = useState(false);
-  const [deviceCount, setDeviceCount] = useState(1);
-  const [deviceList, setDeviceList] = useState([]);
+  const [screenshot, setScreenshot] = useState(null);
+  const [height, setHeight] = useState(1080);
+  const [width, setWidth] = useState(1920);
+  const [query, setQuery] = useState(
+    `https:localhost:3000/api/screenshot?url=${url}&width=1920&height=1080`
+  );
+  const [device, setDevice] = useState({
+    id: 1,
+    name: "IPhone 6, 6s, 7, 8",
+    width: 414,
+    height: 736,
+  });
+  const [discard, setDiscard] = useState(false);
+  const [disableJS, setDisableJS] = useState(false);
+  const [fullPage, setFullPage] = useState(false);
+  const [fileType, setFileType] = useState({ id: 1, name: "png" });
   const [deviceToggle, setDeviceToggle] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoad] = useState(false);
+  const devices = [
+    { id: "iphone6", name: "IPhone 6, 6s, 7, 8", width: 414, height: 736 },
+    { id: "iphone11", name: "IPhone 11", width: 1920, height: 1080 },
+    { id: "googlepixel", name: "Google Pixel", width: 411, height: 731 },
+    { id: "iphonexsmax", name: "IPhone 11 Pro Max, Xs Max", width: 1920, height: 1080 },
+    { id: "macbookpro15", name: "MacBook Pro 15-inch", width: 1920, height: 1080 },
+  ];
+  const fileTypes = [
+    { id: 1, name: "png" },
+    { id: 2, name: "jpeg" }
+  ];
+
+  useEffect(() => {
+    assembleQuery();
+  }, [
+    height,
+    width,
+    deviceToggle,
+    device,
+    url,
+    discard,
+    disableJS,
+    fullPage,
+    fileType,
+  ]);
+
+  const updateOptions = (option, bool) => {
+    switch (option) {
+      case "discard":
+        setDiscard(bool);
+        break;
+      case "disable-js":
+        setDisableJS(bool);
+        break;
+      case "full-page":
+        setFullPage(bool);
+        break;
+      default:
+        break;
+    }
+  };
+
   const getImg = async () => {
     setLoad(true);
-    await fetch(`/api/screenshot?url=${url}`)
-      .then((res) => res.blob())
-      .then((data) => URL.createObjectURL(data))
-      .then((img) => setImg(img));
+    await fetch(query)
+      .then(async (res) => {
+        if (res.ok) {
+          setScreenshot(res.url);
+        } else {
+          let resjson = await res.json()
+          console.log(res)
+          throw new Error(res.status + ' ' + res.statusText + ' : ' + resjson.error );
+        }
+      })
+      .catch((e) => alert(e));
     setLoad(false);
   };
-  const addDevice = () => {
-    const arr = deviceList.slice();
-    arr.push({
-      id: 1,
-      device: "IPhone 6, 6s, 7, 8",
-      width: 1920,
-      height: 1080,
-    });
-    setDeviceList(arr);
+
+  const assembleQuery = () => {
+    let newQuery = `http://localhost:3000/api/screenshot?url=${url}`;
+    if (deviceToggle) {
+      newQuery += `&device=${device.id}`;
+    }
+    if (!deviceToggle) {
+      newQuery += `&width=${width}&height=${height}`;
+    }
+    if (fileType.name != "png") {
+      newQuery += `&filetype=${fileType.name}`;
+    }
+    if (disableJS) {
+      newQuery += "&enablejs=false";
+    }
+    if (discard) {
+      newQuery += "&discard=true";
+    }
+    if (fullPage) {
+      newQuery += "&fullpage=true";
+    }
+
+    setQuery(newQuery);
   };
-  const updateDeviceArr = (e, index) => {
-    const arr = deviceList.slice();
-    console.log(arr);
-    console.log(index);
-    arr[index] = e;
-    setDeviceList(arr);
-    console.log(e);
-    console.log(deviceList);
-  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
       <Head>
         <title>screenshot tool</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-4xl mb-6 font-semibold text-gray-800">Try it out live</h1>
-      <div className="w-1/4 mx-auto bg-gray-100 p-6 border rounded-lg">
-        <h1 className="text-xl mb-4 font-medium text-gray-700">Build your API query</h1>
-      
-          <InputURL label={"URL"} update={setUrl} />
+      <h1 className="text-4xl mb-6 font-semibold text-gray-800">
+        Try it out live
+      </h1>
+      <div className=" max-w-6xl w-auto  bg-gray-100 p-5 border rounded-lg transition-transform">
+        <h1 className="text-xl mb-4 font-medium text-gray-700">
+          Build your API query
+        </h1>
 
-          
-      
+        <InputURL label={"URL"} update={setUrl} />
+
         {deviceToggle ? null : (
-          <div className="my-4">
+          <div className="my-2">
             <h1 className="text-sm font-medium text-gray-700">Browser size</h1>
-            <div className="flex space-x-2  ">
-              <BasicInput label="Width" placeholder="1920" />
-              <BasicInput label="Height" placeholder="1080" />
+            <div className="flex space-x-2 mt-1 ">
+              <BasicInput update={setWidth} label="Width" placeholder={width} />
+              <BasicInput
+                update={setHeight}
+                label="Height"
+                placeholder={height}
+              />
             </div>
           </div>
         )}
-        {deviceToggle ? <Dropdown label="Device" /> : null}
+        {deviceToggle ? (
+          <Dropdown
+            label="Device"
+            update={setDevice}
+            data={device}
+            options={devices}
+          />
+        ) : null}
         <Toggle
           enabled={deviceToggle}
           setEnabled={setDeviceToggle}
           head="Use device"
-          description="Use a specified device as a viewport instead of setting a height and width."
+          description="Use a device for viewport size instead of specifying a height and width."
         />
+        <button
+          onClick={() => setOpen(!open)}
+          className=" mt-3 inline-flex items-center px-2.5 py-2 border border-transparent text-sm font-medium tracking-wide rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          {open ? "Hide advanced options" : "Show advanced options"}
+        </button>
+        {open ? (
+          <div>
+            <Dropdown
+              label="File type"
+              update={setFileType}
+              data={fileType}
+              options={fileTypes}
+            />
+            <Radio
+              discard={discard}
+              disableJS={disableJS}
+              fullPage={fullPage}
+              update={updateOptions}
+            />
+          </div>
+        ) : null}
 
         <label
           htmlFor="about"
-          className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+          className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2 "
         >
           Your API query
         </label>
         <div className="mt-1 ">
           <textarea
+            value={query}
             readOnly
             id="about"
             name="about"
             rows={3}
             className=" shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-            defaultValue={""}
           />
         </div>
 
-        <div className="justify-between flex mt-4">
-        <button
-        type="button"
-        className="inline-flex items-center px-2.5 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Copy URL to Clipboard
-      </button>
-            <button
-              onClick={() => {
-                getImg();
-              }}
-              className=" inline-flex items-center px-2.5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              {loading ? (
-                <svg
-                  className="animate-spin -ml-1 mr-2 h-5 w-5 text-gray-200"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              ) : null}
-              {loading ? "Processing" : "Take Screenshot"}
-            </button>
-          </div>
-        
+        <div className="justify-between flex mt-4 space-x-2">
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(query);
+            }}
+            type="button"
+            className="inline-flex items-center px-2.5 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Copy URL to Clipboard
+          </button>
+          <button
+            onClick={
+              screenshot
+                ? () => {
+                    window.open(screenshot);
+                    setScreenshot(null);
+                  }
+                : () => {
+                    getImg();
+                  }
+            }
+            className=" inline-flex items-center px-2.5 py-2 border border-transparent text-sm font-medium tracking-wide rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            {loading ? (
+              <svg
+                className="animate-spin -ml-1 mr-2 h-5 w-5 text-gray-200"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            ) : null}
+            {loading
+              ? "Processing"
+              : screenshot
+              ? "Open Screenshot"
+              : "Take Screenshot"}
+          </button>
+        </div>
       </div>
     </div>
   );
