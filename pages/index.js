af8c4859556565bff0610e1d7cdcbd918009bd60
skrt-dev <jacobschwantes@outlook.com>
2021-09-12 00:40:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   const [height, setHeight] = useState(1080);
   const [width, setWidth] = useState(1920);
   const [query, setQuery] = useState(
-    `${window.location.href}/api/screenshot?url=${url}&width=1920&height=1080`
+    `/api/screenshot?url=${url}&width=1920&height=1080`
   );
   const [device, setDevice] = useState({
     id: "iphone6",
