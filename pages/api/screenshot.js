5141f1ae1061b588f56c80f335c7430396cbb0ba
skrt-dev <jacobschwantes@outlook.com>
2021-09-14 22:44:20 -0500

    update packages

--- pages/api/screenshot.js
+++ pages/api/screenshot.js
@@ -1,5 +1,7 @@
+
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const puppeteer = require("puppeteer");
+const playwright = require("playwright-aws-lambda");
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -15,79 +17,35 @@ const devices = [
 ];
 
 
-export default async function screenshotAPI(req, res) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      '--no-sandbox',
-    ],
-    defaultViewport: {
-      width:  1280,
-      height: 720,
-    },
-  });
-  const page = await browser.newPage();
-  req.query.enablejs
-    ? await page.setJavaScriptEnabled({ enabled: req.query.enablejs })
-    : null;
-  let device = req.query.device ? devices.find(x => x.name === req.query.device) : null;
-  await page.setViewport({
-      width: device ? device.width : parseInt(req.query.width),
-      height: device ? device.height : parseInt(req.query.height), 
-      isMobile: device ? device.mobile : false
-    })
-    .then(async () => {
-      await page.goto(`https://${req.query.url}`, {
-        waitUntil: "networkidle0",
-    })}
-    )
-    .then(async () => {
-      const image = await page.screenshot({
-        type: req.query.filetype ? req.query.filetype : null,
-        encoding: req.query.discard === 'true' ? 'binary' : "base64",
-        fullPage: req.query.fullpage === 'true' ? true : null
-      });
-      if (req.query.discard === 'true') {
-        res.setHeader("Content-Type", `image/${req.query.filetype ? req.query.filetype : 'png'}`);
-        res.setHeader("Content-Disposition", "inline;");
-        res.status(200).send(image)
-        throw new Error('discard')
-      } else {
-        return image;
+module.exports = async (req, res) => {
+  let browser = null
+  const { query } = req
+  try {
+      if (query.url && isValidUrl(query.url)) {
+          browser = await playwright.launchChromium({ headless: true })
+          const context = await browser.newContext()
+          const page = await context.newPage()
+          await page.goto(query.url)
+          const screenshot = await page.screenshot({ type: "png" })
+          res.setHeader("Content-Type", "image/png")
+          res.status(200).send(screenshot)
+      } else throw "Please provide a valid url"
+  } catch (error) {
+      res.status(500).send({
+          status: "Failed",
+          error,
+      })
+  } finally {
+      if (browser !== null) {
+          await browser.close()
       }
-    })
-    .then((result) => {
-      const URI = `data:image/${req.query.filetype ? req.query.filetype : 'png'};base64,` + result;
-      return URI;
-    })
-    .then(async (uri) => {
-      await cloudinary.uploader.upload(
-        uri,
-        {
-          overwrite: true,
-        },
-        function (error, result) {
-          res.status(200).redirect(result.url);
-        }
-      );
-    })
-    .catch((e) => {
-      let response = null;
-      switch(e.message) {
-        case 'Protocol error (Emulation.setDeviceMetricsOverride): Invalid parameters Failed to deserialize params.width - BINDINGS: int32 value expected at position 21':
-          response = 'Invalid viewport dimensions';
-          break;
-        case 'Unknown options.type value: jpg':
-          response = 'Invalid file type';
-          break;
-        case 'discard':
-          break;
-        default:
-          response = e.message;
-          break;
-      }
-      response !== null ? res.status(400).json({ error: response }) : null;
-    });
-
-  await browser.close();
+  }
+}
+function isValidUrl(string) {
+  try {
+      new URL(string)
+  } catch (_) {
+      return false
+  }
+  return true
 }
