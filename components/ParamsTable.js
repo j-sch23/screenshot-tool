c17218b53764e76cf8f06e0d4929232ba8ff94d6
jsch <jacobschwantes@outlook.com>
2021-09-20 20:09:45 -0500

    Update ParamsTable.js

--- components/ParamsTable.js
+++ components/ParamsTable.js
@@ -1,32 +1,29 @@
-export default function ParamsTable() {
-    return (
-       
-       
-          <div className="w-full my-4  rounded-lg">
-            <div className="w-full ">
-              <table className="w-full">
-                <thead>
-                  <tr className="text-xs  text-left text-gray-900   border-b border-gray-300">
-                    <th className="py-2">Query Parameter</th>
-                    <th className=" py-2">Type</th>
-                    <th className=" py-2">Required</th>
-                  </tr>
-                </thead>
-                <tbody className="">
-                 
-                    <td className=" py-2 ">
-                      url
-                      <div className="flex items-center text-sm">
-                      </div>
-                    </td>
-                 
-                    <td className="py-2 text-sm ">String</td>
-                    <td className="py-2 text-sm ">Optional</td>
-              
-                 
-                </tbody>
-              </table>
-            </div>
-          </div>
-    )
+export default function ParamsTable(props) {
+  return (
+    <div className="w-full my-4  rounded-lg">
+      <div className="w-full ">
+        <table className="w-full">
+          <thead>
+            <tr className="text-xs  text-left text-gray-900   border-b border-gray-300">
+              <th className="py-2">Query Parameter</th>
+              <th className=" py-2">Type</th>
+              <th className=" py-2">Required</th>
+            </tr>
+          </thead>
+
+          {props.data.map((data) => {
+            return (
+              <tbody className="">
+                <td className=" py-2 text-sm ">
+                  <code>{data.param}</code>
+                </td>
+                <td className="py-2 text-sm ">{data.type}</td>
+                <td className="py-2 text-sm italic ">{data.required}</td>{" "}
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
+    </div>
+  );
 }
