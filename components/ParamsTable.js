3069e5629fd277d8b443c28fb0336ad22b873612
skrt-dev <jacobschwantes@outlook.com>
2021-09-27 15:16:08 -0500

    spacing

--- components/ParamsTable.js
+++ components/ParamsTable.js
@@ -5,21 +5,21 @@ export default function ParamsTable(props) {
         <table className="w-full">
           <thead>
             <tr className="text-xs  text-left text-gray-900   border-b border-gray-300">
-              <th className="py-2">Query Parameter</th>
-              <th className=" py-2">Type</th>
-              <th className=" py-2">Required</th>
+              <th className="py-2 px-1">Query Parameter</th>
+              <th className="px-1 py-2">Type</th>
+              <th className="px-1 py-2">Required</th>
             </tr>
           </thead>
 
           {props.data.map((data) => {
             return (
               <tbody className="">
-                <td className=" py-2 text-sm ">
-                  <code>{data.param}</code>
+                <td className=" py-2 px-1  ">
+                  <code className="text-sm font-bold">{data.param}</code>
                   <p className="text-xs">{data.description}</p>
                 </td>
-                <td className="py-2 text-sm ">{data.type}</td>
-                <td className="py-2 text-sm italic ">{data.required}</td>{" "}
+                <td className="py-2 px-1 text-sm ">{data.type}</td>
+                <td className={"py-2 px-1 text-sm " + (data.required === 'optional' ? "italic" : null)}>{data.required}</td>{" "}
               </tbody>
             );
           })}
